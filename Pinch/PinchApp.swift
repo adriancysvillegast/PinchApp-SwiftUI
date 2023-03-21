17a9c37eec7f6caec702a6aca5222e04d0892d58
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-03-21 12:16:01 -0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Adriancys Jesus Villegas Toro on 21/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
